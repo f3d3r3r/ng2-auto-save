f60be851280790a0f7e8fe525b0d1ad5a9ec59ef
lnardai <laci.nardai@gmail.com>
2017-04-29 22:24:55 +0200

    added angular2 option with atosave functionality

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -9,10 +9,23 @@ export class AppComponent {
   title = 'Fancy demo page for ng2-auto-save';
   hint = 'Enter text here!!!';
   textData = '';
+  colors = [
+    {name:'black', shade:'dark'},
+    {name:'white', shade:'light'},
+    {name:'red', shade:'dark'},
+    {name:'blue', shade:'dark'},
+    {name:'yellow', shade:'light'}
+  ];
+  selectedColor = "";
 
   setText(text: string) {
-    console.log("settext called: ", text)
+    console.log("setText called: ", text)
     this.textData = text;
+  }
+
+  setColor(color) {
+    console.log("setColor called: ", color.name)
+    this.selectedColor = color;
   }
 
 }
